df88e19a1e56acd09a65b9b1a11309f1b6d65bc5
Drew Pearce <andrew.f.pearce@accenture.com>
2019-12-01 16:13:01 -0500

    prep for day 2

---
+++ 02/run_02.py
@@ -0,0 +1,21 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
+
+from helpers import load_inputs_lines  # noqa E402
+
+
+def main1(inputs):
+    return 'main1'
+
+
+def main2(inputs):
+    return 'main2'
+
+
+if __name__ == "__main__":
+    inputs = load_inputs_lines('02', 'inputs.txt', 'int')
+    result1 = main1(inputs)
+    result2 = main2(inputs)
+    print(result1)
+    print(result2)
---
+++ 02/test_run_02.py
@@ -0,0 +1,17 @@
+from run_02 import main1
+from run_02 import main2
+
+
+TEST_VALS = {
+    'inputs': [],
+    'result1': 'main1',
+    'result2': 'main2'
+}
+
+
+def test_main1():
+    assert main1(TEST_VALS['inputs']) == TEST_VALS['result1']
+
+
+def test_main2():
+    assert main2(TEST_VALS['inputs']) == TEST_VALS['result2']
