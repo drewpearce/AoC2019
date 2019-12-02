e056c1f1baed8a0b3d2e6a8e86ac056132c966ae
Drew Pearce <andrew.f.pearce@accenture.com>
2019-12-02 10:13:51 -0500

    day 2 solution

--- 02/run_02.py
+++ 02/run_02.py
@@ -1,21 +1,74 @@
 import os
 import sys
+
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
 
 from helpers import load_inputs_lines  # noqa E402
 
 
 def main1(inputs):
-    return 'main1'
+    i = 0
+    while True:
+        op_code = inputs[i]
+
+        if op_code == 99:
+            break
+
+        pos1 = inputs[i + 1]
+        pos2 = inputs[i + 2]
+        pos3 = inputs[i + 3]
+        val1 = inputs[pos1]
+        val2 = inputs[pos2]
+
+        if op_code == 1:
+            result = val1 + val2
+        elif op_code == 2:
+            result = val1 * val2
+        else:
+            msg = f'Invalid op code {op_code} at position {i}.\n\n{inputs}'
+            raise Exception(msg)
+
+        inputs[pos3] = result
+        i += 4
+    
+    return inputs
 
 
-def main2(inputs):
-    return 'main2'
+def load_inputs_1():
+    inputs = load_inputs_lines('02', 'inputs.txt')
+    inputs = [int(x) for x in inputs[0].split(',')]
+    inputs[1] = 12
+    inputs[2] = 2
+    return inputs
+
+
+def load_inputs_2():
+    inputs = load_inputs_lines('02', 'inputs.txt')
+    inputs = [int(x) for x in inputs[0].split(',')]
+    return inputs
+
+
+def main2():
+    for x in range(100):
+        for y in range(100):
+            inputs = load_inputs_2()
+            inputs[1] = x
+            inputs[2] = y
+            result = main1(inputs)
+            if result[0] == 19690720:
+                break
+        if result[0] == 19690720:
+            break
+
+    noun = result[1]
+    verb = result[2]
+    print(f'{noun}, {verb}')
+    return noun * 100 + verb
 
 
 if __name__ == "__main__":
-    inputs = load_inputs_lines('02', 'inputs.txt', 'int')
+    inputs = load_inputs_1()
     result1 = main1(inputs)
-    result2 = main2(inputs)
-    print(result1)
+    print(result1[0])
+    result2 = main2()
     print(result2)
--- 02/test_run_02.py
+++ 02/test_run_02.py
@@ -3,15 +3,28 @@ from run_02 import main2
 
 
 TEST_VALS = {
-    'inputs': [],
-    'result1': 'main1',
+    'inputs': [
+        [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
+        [1, 0, 0, 0, 99],
+        [2, 3, 0, 3, 99],
+        [2, 4, 4, 5, 99, 0],
+        [1, 1, 1, 4, 99, 5, 6, 0, 99]
+    ],
+    'result1': [
+        [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50],
+        [2, 0, 0, 0, 99],
+        [2, 3, 0, 6, 99],
+        [2, 4, 4, 5, 99, 9801],
+        [30, 1, 1, 4, 2, 5, 6, 0, 99]
+    ],
     'result2': 'main2'
 }
 
 
 def test_main1():
-    assert main1(TEST_VALS['inputs']) == TEST_VALS['result1']
+    for i,  val in enumerate(TEST_VALS['inputs']):
+        assert main1(val) == TEST_VALS['result1'][i]
 
 
 def test_main2():
-    assert main2(TEST_VALS['inputs']) == TEST_VALS['result2']
+    assert main2(TEST_VALS['inputs'][0]) == TEST_VALS['result2']
