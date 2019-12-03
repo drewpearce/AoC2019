f3fe4f3b1aa26ff7e0f59399cf26cbf0e93828b8
Drew Pearce <andrew.f.pearce@accenture.com>
2019-12-03 15:45:05 -0500

    add Timer class to helpers

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 
 def load_inputs_lines(folder, file_name, out_type=None):
@@ -14,3 +15,23 @@ def load_inputs_lines(folder, file_name, out_type=None):
         out = [int(o) for o in out]
 
     return out
+
+
+class Timer(object):
+    def __init__(self):
+        self.start_time = self.now()
+        self.stop_time = None
+        self.duration = None
+
+    @staticmethod
+    def now():
+        return int(round(time.time() * 1000))
+
+    def start(self):
+        self.start_time = self.now()
+        self.stop_time = None
+        self.duration = None
+
+    def stop(self):
+        self.stop_time = self.now()
+        self.duration = self.stop_time - self.start_time
