9d18775e72d5f9c42a0351e06def3e00b330fe99
Drew Pearce <andrew.f.pearce@accenture.com>
2019-12-04 10:41:06 -0500

    update template

--- template/run_xx.py
+++ template/run_xx.py
@@ -3,19 +3,32 @@ import sys
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
 
 from helpers import load_inputs_lines  # noqa E402
+from helpers import Timer  # noqa E402
 
 
-def main1(inputs):
+def main1(data=None):
+    if not data:
+        data = load_inputs_lines('xx', 'inputs.txt', 'int')
+
     return 'main1'
 
 
-def main2(inputs):
+def main2(data=None):
+    if not data:
+        data = load_inputs_lines('xx', 'inputs.txt', 'int')
+
     return 'main2'
 
 
 if __name__ == "__main__":
-    inputs = load_inputs_lines('xx', 'inputs.txt', 'int')
-    result1 = main1(inputs)
-    result2 = main2(inputs)
+    timer = Timer()
+    result1 = main1()
+    timer.stop()
     print(result1)
+    print(f'{timer.duration} ms')
+
+    timer.start()
+    result2 = main2()
+    timer.stop()
     print(result2)
+    print(f'{timer.duration} ms')
--- template/test_run_xx.py
+++ template/test_run_xx.py
@@ -3,15 +3,19 @@ from run_xx import main2
 
 
 TEST_VALS = {
-    'inputs': [],
-    'result1': 'main1',
-    'result2': 'main2'
+    'inputs': [
+        ['test']
+    ],
+    'result1': ['main1'],
+    'result2': ['main2']
 }
 
 
 def test_main1():
-    assert main1(TEST_VALS['inputs']) == TEST_VALS['result1']
+    for i, val in enumerate(TEST_VALS['inputs']):
+        assert main1(val) == TEST_VALS['result1'][i]
 
 
 def test_main2():
-    assert main2(TEST_VALS['inputs']) == TEST_VALS['result2']
+    for i, val in enumerate(TEST_VALS['inputs']):
+        assert main2(val) == TEST_VALS['result2'][i]
